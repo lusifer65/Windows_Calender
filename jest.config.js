5813bf2dab622474e87cece91c7c6cdb396cfc77
Akash <akash.verma.contractor@move.com>
2025-06-30 02:53:28 +0530

    Refactor Babel and Jest configurations for improved testing setup

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ module.exports = {
   testEnvironment: 'jsdom',  // Enables DOM testing for React components
   setupFilesAfterEnv: ['@testing-library/jest-dom'], // Adds custom matchers
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest', // Transform JS/JSX files with Babel
+    '^.+\\.(js|jsx)$': ['babel-jest', {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        ['@babel/preset-react', { runtime: 'automatic' }]
+      ]
+    }], // Transform JS/JSX files with Babel
   },
   moduleFileExtensions: ['js', 'jsx', 'json'],
   testMatch: [
